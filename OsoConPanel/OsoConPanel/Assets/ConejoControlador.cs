3ad65f3ee7b406746b0d6d47275c230407077bf0
lpla14 <leilaplanisi@hotmail.com>
2018-08-20 15:57:08 -0300

    Nueva escena animales + movimiento en FPSController
    Nueva escena: cambio de terreno, más animales. Alces con interacción al apretar C (camina o deja de caminar)
    Se agrega script al FPSController de la escena AmbienteScene (el gameobject es Player) para controlar el movimiento del player desde el joystick

---
+++ OsoConPanel/OsoConPanel/Assets/ConejoControlador.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConejoControlador : MonoBehaviour {
+    Animator conejoAnimator;
+	// Use this for initialization
+	void Start () {
+        conejoAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (conejoAnimator.GetBool("Saltando"))
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+	}
+}
---
+++ OsoConPanel/OsoConPanel/Assets/Controladores de animaciones/AlceInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlceInteraction : MonoBehaviour {
+
+    Animator alceAnimator;
+	// Use this for initialization
+	void Start () {
+        alceAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Joystick1Button2)) {
+            alceAnimator.SetBool("Caminar", !alceAnimator.GetBool("Caminar"));
+
+            alceAnimator.SetBool("Comiendo", !alceAnimator.GetBool("Caminar"));
+
+            
+
+        }
+        if (alceAnimator.GetBool("Caminar")) {
+            transform.position += new Vector3(-0.06F, 0, 0);
+        }
+	}
+}
