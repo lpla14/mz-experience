d698cc11c8b1693bffa61916263649b81d2feb6a
lpla14 <leilaplanisi@hotmail.com>
2018-08-20 19:49:34 -0300

    Agreado de elementos de bosque, loop animaciones,
    rocas, texturas, eliminacion de estados innecesarios de animaciones, cesped

--- OsoConPanel/OsoConPanel/Assets/Controladores de animaciones/AlceInteraction.cs
+++ OsoConPanel/OsoConPanel/Assets/Controladores de animaciones/AlceInteraction.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class AlceInteraction : MonoBehaviour {
 
     Animator alceAnimator;
-	// Use this for initialization
-	void Start () {
+
+    private string estadoCaminar = "Caminar";
+    private string estadoComer   = "Comiendo";
+
+    // Use this for initialization
+    void Start () {
         alceAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.Joystick1Button2)) {
-            alceAnimator.SetBool("Caminar", !alceAnimator.GetBool("Caminar"));
+            alceAnimator.SetBool(estadoCaminar, !alceAnimator.GetBool(estadoCaminar));
 
-            alceAnimator.SetBool("Comiendo", !alceAnimator.GetBool("Caminar"));
-
+            alceAnimator.SetBool(estadoComer, !alceAnimator.GetBool(estadoCaminar));
             
-
         }
-        if (alceAnimator.GetBool("Caminar")) {
+        if (alceAnimator.GetBool(estadoCaminar)) {
             transform.position += new Vector3(-0.06F, 0, 0);
         }
 	}
---
+++ OsoConPanel/OsoConPanel/Assets/Controladores de animaciones/DoeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoeController : MonoBehaviour {
+    Animator doeAnimator;
+    // Use this for initialization
+    void Start()
+    {
+        doeAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doeAnimator.GetBool("Caminando"))
+        {
+            transform.position += new Vector3(0, 0,-0.01F);
+        }
+    }
+}
