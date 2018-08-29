96146c6ac4f7d57c3765c0d60602338acb47b051
lpla14 <leilaplanisi@hotmail.com>
2018-08-28 23:42:43 -0300

    Botones menu y fix tamaño background pantalla inicio
    Se agregan todos los botones de menú interactuar + cambio de estilo.
    Fix tamaño de la imagen de background en la pantalla de inicio

--- OsoConPanel/OsoConPanel/Assets/MyScripts/AnimalMenu.cs
+++ OsoConPanel/OsoConPanel/Assets/MyScripts/AnimalMenu.cs
@@ -3,69 +3,80 @@ using UnityEngine.UI;
 
 public class AnimalMenu : MonoBehaviour
 {
-    public  GameObject    canvas;
-    public  GameObject    firstPerson;
-   
-    private RectTransform canvasRectTransform;
-    private bool          menuVisible;
+    public GameObject canvas;
+    public GameObject firstPerson;
 
-    private GameObject    bInteractuar;
-	private GameObject    bComer;
-	private GameObject    bDormir;
-	private GameObject    bAcariciar;
-	
-    void Start ()
+    private RectTransform canvasRectTransform;
+    private bool menuVisible;
+
+    private GameObject bInteractuar;
+    private GameObject bComer;
+    private GameObject bAcariciar;
+    private GameObject bVerInformacion;
+    private GameObject bFingirMuerte;
+    private GameObject bDespertar;
+    private GameObject bDormir;
+    private GameObject bCorrer;
+    private GameObject bDeseleccionar;
+
+    void Start()
     {
-        menuVisible         = false;
+        menuVisible = false;
         canvasRectTransform = canvas.GetComponent<RectTransform>();
-		bInteractuar        = GameObject.Find("bInteractuar");
-		bComer 				= GameObject.Find("bComer");
-        bAcariciar          = GameObject.Find("bAcariciar");
-        //bVerInformacion
-        //bFingirMuerte
-        //bDespertar
-        bDormir             = GameObject.Find("bDormir");
-        //bCorrer
-        //bDeseleccionar
-        // bDeseleccionar.GetComponent<Button>().onClick.AddListener(
-        //                 delegate { MostrarMenuInteractuar(false); }
-        //                 );
+        bInteractuar = GameObject.Find("bInteractuar");
+        bComer = GameObject.Find("bComer");
+        bAcariciar = GameObject.Find("bAcariciar");
+        bVerInformacion = GameObject.Find("bVerInformacion");
+        bFingirMuerte = GameObject.Find("bFingirMuerte");
+        bDespertar = GameObject.Find("bDespertar");
+        bDormir = GameObject.Find("bDormir");
+        bCorrer = GameObject.Find("bCorrer");
+        bDeseleccionar = GameObject.Find("bDeseleccionar");
+
+        bDeseleccionar.GetComponent<Button>().onClick.AddListener(
+                delegate { MostrarMenu(false); }
+            );
 
         bInteractuar.GetComponent<Button>().onClick.AddListener(
-                delegate { MostrarMenuInteractuar(true); }
-                );
+                delegate { MostrarMenu(true); }
+            );
 
         //todo ver de setear los event trigger PointerEnter/PointerExit por script       
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-        if ( bInteractuar != null && bInteractuar.activeSelf)
+        if (bInteractuar != null && bInteractuar.activeSelf)
         {
-			
-			canvasRectTransform.localPosition = new Vector3(firstPerson.transform.localPosition.x, firstPerson.transform.localPosition.y + 1, firstPerson.transform.localPosition.z - 3); 
-			
+
+            canvasRectTransform.localPosition = new Vector3(firstPerson.transform.localPosition.x, firstPerson.transform.localPosition.y + 1, firstPerson.transform.localPosition.z - 3);
+
             //A
             if (Input.GetKeyDown(KeyCode.Joystick1Button3))
             {
                 // se activa on pointer enter del animal y se desactiva on pointer exit
-                bInteractuar.SetActive ( false );  
+                bInteractuar.SetActive(false);
             }
 
         }
-	
+
     }
 
-    private void MostrarMenuInteractuar( bool mostrarMenu )
+    private void MostrarMenu(bool mostrarMenu)
     {
-        bInteractuar.SetActive ( !mostrarMenu );
-		
-		bComer.SetActive  ( mostrarMenu );
-		bDormir.SetActive ( mostrarMenu );
-		bAcariciar.SetActive( mostrarMenu );
-     
-		menuVisible = mostrarMenu;
+        bInteractuar.SetActive(false);
+
+        bComer.SetActive(mostrarMenu);
+        bDormir.SetActive(mostrarMenu);
+        bAcariciar.SetActive(mostrarMenu);
+        bVerInformacion.SetActive(mostrarMenu);
+        bFingirMuerte.SetActive(mostrarMenu);
+        bDespertar.SetActive(mostrarMenu);
+        bCorrer.SetActive(mostrarMenu);
+        bDeseleccionar.SetActive(mostrarMenu);
+
+        menuVisible = mostrarMenu;
     }
 
 }
