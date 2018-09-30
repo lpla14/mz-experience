c6453cbcce7437b5bf57b6add59c0fa13effec21
walterezeqiel <walter.e.vega@gmail.com>
2018-09-30 13:28:25 -0300

    Agregado el Boton de Salir y Pantalla General
    al apretar el boton A vuelve al menu principal desde el Artico. Ademas a
    los menu de seleccion y principal se los hizo de tamaño general

---
+++ OsoConPanel/OsoConPanel/Assets/Volver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if (Input.GetKeyDown(Botones.BOTON_A))
+        {
+            var ff= GetComponentInParent<LoadScene>();
+            ff.enabled = true;    
+        }
+	}
+}
