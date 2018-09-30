3a653d60aa98df64b32e2f955d0646733404cf91
walterezeqiel <walter.e.vega@gmail.com>
2018-09-30 19:31:37 -0300

    Modificacion de menu
    Se agregaron huellas

---
+++ OsoConPanel/OsoConPanel/Assets/MyScripts/CargarEscena.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargarEscena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(Botones.BOTON_A) || Input.GetKeyDown(Botones.BOTON_B) ||
+            Input.GetKeyDown(Botones.BOTON_C) || Input.GetKeyDown(Botones.BOTON_D) ||
+            Input.GetKeyDown(Botones.BOTON_R1) || Input.GetKeyDown(Botones.BOTON_R2))
+        {
+            var a = GetComponent<LoadScene>();
+            a.enabled = true;
+        }
+	}
+}
