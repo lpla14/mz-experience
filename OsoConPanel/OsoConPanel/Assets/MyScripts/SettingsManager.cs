410d320d2135116557bb0cbde8bd598b348fd490
reskin7 <43115836+reskin7@users.noreply.github.com>
2018-10-14 20:17:28 -0300

    Menu Opciones
    Se agregó funcionalidad en control de volumen y graficos.

---
+++ OsoConPanel/OsoConPanel/Assets/MyScripts/SettingsManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public AudioMixer audioMixer;
+
+    public void subirVol()
+    {
+        if (volumeSlider.value < 0)
+        {
+            volumeSlider.value += 10;
+            audioMixer.SetFloat("volume",volumeSlider.value);
+        }
+    }
+
+    public void bajarVol()
+    {
+        if (volumeSlider.value > -80)
+        {
+            volumeSlider.value -= 10;
+            audioMixer.SetFloat("volume", volumeSlider.value);
+        }
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+
+    }
+
+}
