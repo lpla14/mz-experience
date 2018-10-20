978d6a51a5734e1db612ef0f069e7fe429be3deb
hernan18 <hernan.silva.18@gmail.com>
2018-10-20 17:44:43 -0300

    Cambios en Bosque y menues
    Se creo la escena MenuPausa.
    Se elimino la escena SampleScene.

--- OsoConPanel/OsoConPanel/Assets/AbrirMenuPausa.cs
+++ OsoConPanel/OsoConPanel/Assets/AbrirMenuPausa.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AbrirMenuPausa : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
