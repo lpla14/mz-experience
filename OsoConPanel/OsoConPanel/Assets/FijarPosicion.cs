b64e300b343bbaf74e273d0a444f95930367ad98
reskin7 <43115836+reskin7@users.noreply.github.com>
2018-10-20 18:35:56 -0300

    Menu ayuda hecho
    se agregaron las imagens en el menu ayuda y se arreglo la posicion del canvas

---
+++ OsoConPanel/OsoConPanel/Assets/FijarPosicion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FijarPosicion : MonoBehaviour {
+    public float x, y, z;
+    
+	// Use this for initialization
+	void Start () {
+        transform.localPosition = new Vector3(x,y,z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
