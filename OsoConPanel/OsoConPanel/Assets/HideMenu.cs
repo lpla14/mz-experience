b6d6bf813c0dae17c440d13e6b9a8a93f3157d89
lpla14 <leilaplanisi@hotmail.com>
2018-10-25 00:00:14 -0300

    Boton R2 (joystick) - B (teclado)  como BACK
    Al presionar estos botones:
    -Cuando interactuar esta visible -> se oculta
    -Cuando el menu de interacciones esta visible -> se oculta
    -Cuando el panel de info esta visible -> se oculta

---
+++ OsoConPanel/OsoConPanel/Assets/HideMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMenu : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update () {
+
+        if ((Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(Botones.BOTON_R2)) && !GetComponent<MenuInteracciones>().enabled)
+        {
+            var bComer = GameObject.Find(Botones.ID_BOTON_COMER);
+
+            if (bComer != null && bComer.activeSelf)
+            {
+                GetComponent<MenuInteracciones>().enabled = true;
+
+                GetComponent<MenuInteracciones>().MostrarMenu(false);
+                GetComponent<MenuInteracciones>().enabled = false;
+            }
+
+        }
+
+    }
+}
