b69ec3527b991ca57eb9860cb0a1247dffa2375a
hernan18 <hernan.silva.18@gmail.com>
2018-10-27 13:29:07 -0300

    Revert "Revert "Accion Acariciar""
    This reverts commit 9db5616bf74e0af5f29d664d66e7d2cebd5c47f4.

--- OsoConPanel/OsoConPanel/Assets/MenuInteracciones.cs
+++ OsoConPanel/OsoConPanel/Assets/MenuInteracciones.cs
@@ -115,7 +115,13 @@ public class MenuInteracciones : MonoBehaviour {
                     GetComponent<EstadoAnimales>().SetDormido(animator.name, false);
                 }
 
-                if ( animator != null && interaccion > -1 )
+                if (interaccion == AnimacionesAnimales.ACCION_ACARICIAR)
+                {
+                    animator.GetComponentInParent<Acariciar>().enabled = true;
+
+                }
+
+                    if ( animator != null && interaccion > -1 )
                 {
                     // Animaciones.Animales es el script que tiene los diccionarios (nombre de animaciones por animal, info de animales)
 
