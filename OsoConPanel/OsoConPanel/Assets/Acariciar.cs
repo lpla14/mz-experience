5c594f2ec2bfe65d5786280ceb5b340b693cf02f
hernan18 <hernan.silva.18@gmail.com>
2018-10-28 11:01:15 -0300

    Revert "Merge pull request #12 from lpla14/Interaccion_Acariciar"
    This reverts commit b6caaa6180b8db73d2f60397fa14c5b9c9df2260, reversing
    changes made to 4409b03da408f83d0b202a02b4a3c50ab0b80dc2.

--- OsoConPanel/OsoConPanel/Assets/Acariciar.cs
+++ OsoConPanel/OsoConPanel/Assets/Acariciar.cs
@@ -2,58 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Acariciar : MonoBehaviour
-{
+public class Acariciar : MonoBehaviour {
 
     private float posInicial;
     private float posFinal;
     private bool adelante = true;
     private bool atras = false;
-    private int cont = 0;
 
-    public GameObject mano;
-
-    // Use this for initialization
-    void Start()
-    {
-
-        mano.SetActive(true);
-        posInicial = mano.transform.position.z;
+	// Use this for initialization
+	void Start () {
+        posInicial = transform.position.z;
         posFinal = posInicial - 1.43f;
-
-    }
+        Debug.Log(posInicial);
+        Debug.Log(posFinal);
+	}
 
     // Update is called once per frame
-    void Update()
-    {
-        if (cont < 1000)
-            StartCoroutine("MoverMano");
+    void Update() {
+        if (transform.position.z > posFinal && adelante == true)
+        { 
+            transform.position -= new Vector3(0,0,0.02f);
+         }
         else
-            mano.SetActive(false);
-
-    }
-
-    IEnumerator MoverMano()
-    {
-        for (int f = 0; f < 3; f += 1)
         {
-            if (mano.transform.position.z > posFinal && adelante == true)
-            {
-                mano.transform.position -= new Vector3(0, 0, 0.008f);
-            }
-            else
-            {
-                adelante = false;
-            }
+            adelante = false;
+         }
 
-            if (mano.transform.position.z < posInicial && adelante == false)
-            {
-                mano.transform.position += new Vector3(0, 0, 0.008f);
-            }
-            else
-                adelante = true;
-            cont++;
-            yield return null;
+        if (transform.position.z < posInicial && adelante == false)
+        {
+            transform.position += new Vector3(0, 0, 0.02f);
         }
+        else
+            adelante = true;
     }
 }
